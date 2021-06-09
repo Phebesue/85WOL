0e97f2ce0410e841593e2813341651ab3f7d7ebc
Phebesue <62977906+Phebesue@users.noreply.github.com>
2021-06-09 15:34:21 -0400

    9.3.2

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const sequelize = require("../db");
 const { UserModel } = require("../models");
 const {UniqueConstraintError}= require("sequelize/lib/errors");
 
