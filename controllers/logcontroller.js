d1059841e1174bd31d2b1caf790d16fb60dc2830
Phebesue <62977906+Phebesue@users.noreply.github.com>
2021-06-14 14:54:39 -0400

    12.3.3 CREATING A JOURNAL ENTRY (POST)

--- controllers/logcontroller.js
+++ controllers/logcontroller.js
@@ -1,8 +1,39 @@
-const Express = require('express');
- const router = Express.Router();
+const Express = require("express");
+const router = Express.Router();
+let validateJWT = require("../middleware/validate-jwt");
 
-router.get('/practice', (req, res) => {
-res.send('Hey! ! This is a practice route!')
+const { LogModel } = require("../models");
+
+router.get("/practice", (req, res) => {
+  res.send("Hey! ! This is a practice route!");
 });
-module.exports = router;
 
+/*
+=========================
+Log Create
+=========================
+*/
+router.post('/create', validateJWT, async (req, res) =>{
+    const {description, definition, result }= req.body.log;
+    const {id}= req.user;
+    const logEntry = {
+        description,
+        definition,
+        result,
+        owner: id
+    }
+    try {
+        const newLog = await LogModel.create(logEntry);
+        res.status(200).json(newLog);
+    }catch (err){
+        res.status(500).json({error:err});
+    }
+    LogModel.create(logEntry)
+
+});
+
+router.get("/about", (req, res)=> {
+    res.send("This is the about route!")
+});
+
+module.exports = router;
